5c024842598fcce1bbb6871ca756d8e099a47412
aibabaimukhan <159976285+aibabaimukhan@users.noreply.github.com>
2025-02-17 15:56:40 +0500

    Update api_service.dart

--- bd_flutter/api_service.dart
+++ bd_flutter/api_service.dart
@@ -1,21 +1,29 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'dart:convert'; // Импорт библиотеки для работы с JSON
+import 'package:http/http.dart' as http; // Импорт пакета http для выполнения HTTP-запросов
 
-class ApiService {
-  static const String baseUrl = "http://flutter";
+class ApiService { // Определение класса ApiService
+  static const String baseUrl = "http://flutter"; // Определение базового URL для API
 
-  static Future<List<Map<String, dynamic>>> fetchUsers() async {
-    final response = await http.get(Uri.parse('$baseUrl/get_user.php'));
+  // Метод для получения списка пользователей
+  static Future<List<Map<String, dynamic>>> fetchUsers() async { // Определение асинхронного метода fetchUsers
+    final response = await http.get(Uri.parse('$baseUrl/get_user.php')); // Выполнение GET-запроса к API
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      if (data['status'] == 'success') {
-        return List<Map<String, dynamic>>.from(data['users']);
-      } else {
-        throw Exception("Ошибка: ${data['message']}");
+    // Проверка, что запрос выполнен успешно (статус код 200)
+    if (response.statusCode == 200) { 
+      // Декодирование тела ответа из JSON в объект Dart
+      final data = json.decode(response.body); 
+
+      // Проверка, что статус в ответе "success"
+      if (data['status'] == 'success') { 
+        // Преобразование списка пользователей из JSON в список Map в Dart
+        return List<Map<String, dynamic>>.from(data['users']); 
+      } else { 
+        // Если статус не "success", выбрасывается исключение с сообщением об ошибке
+        throw Exception("Ошибка: ${data['message']}"); 
       }
-    } else {
-      throw Exception("Ошибка соединения: ${response.statusCode}");
+    } else { 
+      // Если статус код не 200, выбрасывается исключение с кодом ошибки
+      throw Exception("Ошибка соединения: ${response.statusCode}"); 
     }
   }
 }
